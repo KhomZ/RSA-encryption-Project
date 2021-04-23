36317ca673dd2fdbf50cee226d7ba170acf95a58
KhomZ <54626716+KhomZ@users.noreply.github.com>
2021-04-23 21:04:13 +0545

    created shors.py

---
+++ shors.py
@@ -0,0 +1,22 @@
+"""
+@author: KhomZ
+  @kyzen khom
+"""
+
+from qiskit.aqua.algorithms import Shor
+from qiskit.aqua import QuantumInstance
+from qiskit import Aer
+
+key = 21 # no. to factor
+base = 2
+
+backend = Aer.get_backend('qasm_simulator')
+
+qi = QuantumInstance(backend=backend, shots=1024)
+
+shors = Shor(N=key, a=base, quantum_instance=qi)
+
+results = shors.run()
+print(result['factors'])
+
+@ikhomkodes
